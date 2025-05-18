e66c8da501b562f87d3c754fbb644852d1138921
Naufal <naufalprakasa78@gmail.com>
2025-05-18 17:01:33 +0700

    feat: reorganize route imports to clarify public and admin route structure

--- backend/app.ts
+++ backend/app.ts
@@ -5,9 +5,14 @@
     import cookieParser from 'cookie-parser'; 
 
 
+    //admin routes
     import adminRoutes from './src/routes/adminRoutes';
-    import competitionsRoutes from "./src/routes/competitionsRoutes";
-    import participantRoutes from "./src/routes/participantRoutes";
+    import competitionsRoutes from "./src/routes/admin/competitionsRoutes";
+    import participantRoutes from "./src/routes/admin/participantRoutes";
+
+    //public routes
+    import pubclicCompetitionRoutes from "./src/routes/public/publicCompetitionRoutes";
+    import publicParticipantRoutes from "./src/routes/public/publicParticipantRoutes";
 
     const corsOptions = {
       origin: ['http://localhost:5501', 'http://localhost:5500'],
@@ -23,7 +28,12 @@
 
     setupSwagger(app);
 
+    //public routes
+    app.use('/public/api', pubclicCompetitionRoutes);
+    app.use('/public/api', publicParticipantRoutes);
+
     
+    //admin routes
     app.use('/admin', adminRoutes);
     app.use("/admin/api", competitionsRoutes);
     app.use("/admin/api", participantRoutes)
